1051fe5c8cecf5692341684080f9d8e0664fac68
heather dougherty <heatherdougherty1234@me.com>
2020-12-23 23:24:10 -0500

    fix formatting

--- AscendingNumbers/AscendingNumberValidator.cs
+++ AscendingNumbers/AscendingNumberValidator.cs
@@ -2,28 +2,28 @@
 
 namespace AscendingNumbers
 {
-	public class AscendingNumberValidator
-	{
-		/// <summary>
-		/// Returns true if the numbers array parameter contains a set of consecutive ascending numbers,
-		/// otherwise return false.
-		/// </summary>
-		public bool IsAscending(int[] numbers)
-		{ 
-			if(numbers.Length == 0)
+  public class AscendingNumberValidator
+  {
+    /// <summary>
+    /// Returns true if the numbers array parameter contains a set of consecutive ascending numbers,
+    /// otherwise return false.
+    /// </summary>
+    public bool IsAscending(int[] numbers)
+    {
+      if (numbers.Length == 0)
       {
-				return false;
+        return false;
       }
 
-			for (int num = 0; num < numbers.Length - 1; num++)
+      for (int num = 0; num < numbers.Length - 1; num++)
       {
-				if (numbers[num] + 1 != numbers[num + 1])
+        if (numbers[num] + 1 != numbers[num + 1])
         {
-					return false;
+          return false;
         }
       }
 
-			return true;
-		}
-	}
+      return true;
+    }
+  }
 }
